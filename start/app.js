1362b67ac2d6e5f635d792709d2af580dfac832d
FlavioBS <bzsflavio@gmail.com>
2019-11-21 18:30:44 -0300

    recuperaçao de senha / envio de email

--- start/app.js
+++ start/app.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 |--------------------------------------------------------------------------
@@ -11,12 +11,14 @@
 |
 */
 const providers = [
-  '@adonisjs/framework/providers/AppProvider',
-  '@adonisjs/auth/providers/AuthProvider',
-  '@adonisjs/bodyparser/providers/BodyParserProvider',
-  '@adonisjs/cors/providers/CorsProvider',
-  '@adonisjs/lucid/providers/LucidProvider'
-]
+  "@adonisjs/framework/providers/AppProvider",
+  "@adonisjs/auth/providers/AuthProvider",
+  "@adonisjs/bodyparser/providers/BodyParserProvider",
+  "@adonisjs/cors/providers/CorsProvider",
+  "@adonisjs/lucid/providers/LucidProvider",
+  "@adonisjs/mail/providers/MailProvider",
+  "@adonisjs/framework/providers/ViewProvider"
+];
 
 /*
 |--------------------------------------------------------------------------
@@ -27,9 +29,7 @@ const providers = [
 | Providers for migrations, tests etc.
 |
 */
-const aceProviders = [
-  '@adonisjs/lucid/providers/MigrationsProvider'
-]
+const aceProviders = ["@adonisjs/lucid/providers/MigrationsProvider"];
 
 /*
 |--------------------------------------------------------------------------
@@ -43,7 +43,7 @@ const aceProviders = [
 |   { Route: 'Adonis/Src/Route' }
 |
 */
-const aliases = {}
+const aliases = {};
 
 /*
 |--------------------------------------------------------------------------
@@ -53,6 +53,6 @@ const aliases = {}
 | Here you store ace commands for your package
 |
 */
-const commands = []
+const commands = [];
 
-module.exports = { providers, aceProviders, aliases, commands }
+module.exports = { providers, aceProviders, aliases, commands };
--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,9 @@
 "use strict";
 
+const Route = use("Route");
+
 Route.post("users", "UserController.store");
 Route.post("session", "SessionController.store");
+
+Route.post("passwords", "ForgotPasswordController.store");
+Route.put("passwords", "ForgotPasswordController.update");

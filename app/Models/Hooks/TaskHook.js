27dbc044d143830712886b0b1f2c43e0bd8b830a
FlavioBS <bzsflavio@gmail.com>
2019-11-22 12:56:46 -0300

    fila com redis

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const Mail = use("Mail");
-const Helpers = use("Helpers");
+const Kue = use("Kue");
+const Job = use("App/Jobs/NewTaskMail");
 
 const TaskHook = (exports = module.exports = {});
 
@@ -15,20 +15,5 @@ TaskHook.sendNewTaskMail = async taskInstance => {
 
   const { title } = taskInstance;
 
-  await Mail.send(
-    ["emails.new_task"],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message
-        .to(email)
-        .from("bzsflavio@gmail.com", "FlavioBS | FBS")
-        .subject("Nova tarefa para você");
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        });
-      }
-    }
-  );
+  Kue.dispatch(Job.key, { email, username, file, title }, { attempts: 3 });
 };

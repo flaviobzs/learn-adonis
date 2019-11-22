03338c6df2e671a1993004a9fde2f38b245bbb0e
FlavioBS <bzsflavio@gmail.com>
2019-11-21 23:36:50 -0300

    excessoes de erros

--- start/routes.js
+++ start/routes.js
@@ -2,7 +2,7 @@
 
 const Route = use("Route");
 
-Route.post("users", "UserController.store");
+Route.post("users", "UserController.store").validator("User");
 Route.post("session", "SessionController.store");
 
 Route.post("passwords", "ForgotPasswordController.store");
